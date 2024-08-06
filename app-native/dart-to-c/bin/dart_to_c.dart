88b0b8a0605317e787fdf8383e1a3015c012a421
Hugh Wang <hghwng@gmail.com>
2024-08-06 20:41:52 +0800

    app: add dart (template)

---
+++ app-native/dart-to-c/bin/dart_to_c.dart
@@ -0,0 +1,5 @@
+import 'package:dart_to_c/dart_to_c.dart' as dart_to_c;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_to_c.calculate()}!');
+}
---
+++ app-native/dart-to-c/test/dart_to_c_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_to_c/dart_to_c.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
