4778769b49d9614bd9933c2758cfcbffe8af4e63
Hugh Wang <hghwng@gmail.com>
2024-08-06 16:11:45 +0800

    app: add native benchmark

---
+++ app-native/c-to-c.cc
@@ -0,0 +1,40 @@
+#include <chrono>
+#include <cstdio>
+#include <random>
+#include <string>
+
+#include "../lib/c_api.h"
+
+// Computes the average execution duration (nano seconds) for "char
+// *OH_Concat(const char **args)"
+unsigned benchmark(unsigned num_strs, unsigned length_per_str,
+                   unsigned num_trials) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<char> dis('a', 'z');
+
+  std::vector<std::string> args;
+  std::vector<const char *> argp;
+
+  for (unsigned i = 0; i < num_strs; ++i) {
+    std::string str(length_per_str, dis(gen));
+    args.push_back(std::move(str));
+    argp.push_back(args.back().c_str());
+  }
+  argp.push_back(nullptr);
+
+  auto start = std::chrono::high_resolution_clock::now();
+  for (unsigned i = 0; i < num_trials; ++i) free(OH_Concat(argp.data()));
+  auto end = std::chrono::high_resolution_clock::now();
+
+  auto duration =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+  return duration.count() / num_trials;
+}
+
+int main() {
+  for (unsigned i = 0; i < 5000; i += 200) {
+    printf("%8u,%8u\n", i, benchmark(1500, i, 3));
+  }
+  return 0;
+}
