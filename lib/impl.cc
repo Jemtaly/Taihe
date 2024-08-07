81550c451f2fd7aba613b302fabf5eded794cc11
Hugh Wang <hghwng@gmail.com>
2024-08-07 19:36:02 +0800

    lib: add taihe implementation

--- lib/impl.cc
+++ lib/impl.cc
@@ -3,11 +3,14 @@
 #include <string>
 #include <vector>
 
+#include "taihe_rt.h"
+
 namespace api_impl {
 int plus(int a, int b) { return a + b; }
 
 std::string concat(std::vector<std::string> xs) {
   std::string ret;
+
   for (const auto &x : xs) {
     if (x.empty()) continue;
     ret += x;
@@ -21,3 +24,24 @@ std::string concat(std::vector<std::string> xs) {
 }
 
 }  // namespace api_impl
+
+namespace api_taihe_impl {
+using taihe::TList;
+using taihe::TString;
+
+TString *concat(TList *xs) {
+  std::string ret;
+
+  for (const auto &o : *xs) {
+    TString *x = o.cast<TString>();
+    if (x->empty()) continue;
+    ret += *x;
+    ret += ';';
+  }
+
+  // Remove the trailing separator ';'.
+  if (!ret.empty()) ret.pop_back();
+
+  return new TString(std::move(ret));
+}
+}  // namespace api_taihe_impl
--- lib/impl.h
+++ lib/impl.h
@@ -2,7 +2,16 @@
 #include <string>
 #include <vector>
 
+#include "taihe_rt.h"
+
 namespace api_impl {
 int plus(int a, int b);
 std::string concat(std::vector<std::string> xs);
 }  // namespace api_impl
+
+namespace api_taihe_impl {
+using taihe::TList;
+using taihe::TString;
+
+TString *concat(TList *xs);
+}  // namespace api_taihe_impl
