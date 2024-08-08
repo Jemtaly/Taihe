b66d5495da179f5c428769ca3bb103f159dd149f
Hugh Wang <hghwng@gmail.com>
2024-08-08 19:34:18 +0800

    app: measure cpp-to-cpp

--- app-native/c-to-c.cc
+++ app-native/c-to-c.cc
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "../lib/c_api.h"
+#include "../lib/impl.h"
 
 // Computes the average execution duration (nano seconds) for "char
 // *OH_Concat(const char **args)"
@@ -28,15 +29,46 @@ unsigned benchmark(unsigned num_strs, unsigned length_per_str,
   return duration.count() / num_trials;
 }
 
+// Computes the average execution duration (nano seconds) for "char
+// *OH_Concat(const char **args)"
+unsigned benchmark_cpp(unsigned num_strs, unsigned length_per_str,
+                       unsigned num_trials) {
+  std::vector<std::string> args;
+
+  for (unsigned i = 0; i < num_strs; ++i) {
+    std::string str(length_per_str, 'x');
+    args.push_back(std::move(str));
+  }
+
+  auto start = std::chrono::high_resolution_clock::now();
+  for (unsigned i = 0; i < num_trials; ++i) api_impl::concat(args);
+  auto end = std::chrono::high_resolution_clock::now();
+
+  auto duration =
+      std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  return duration.count() / num_trials;
+}
+
 void run_once(unsigned num_strs, unsigned length_per_str, unsigned num_trials) {
   printf("tbench:c-capi,%u,%u,%u\n", num_strs, length_per_str,
          benchmark(num_strs, length_per_str, num_trials));
+}
+
+void run_cpp_once(unsigned num_strs, unsigned length_per_str,
+                  unsigned num_trials) {
+  printf("tbench:cpp-cpp,%u,%u,%u\n", num_strs, length_per_str,
+         benchmark_cpp(num_strs, length_per_str, num_trials));
 }
 
 int main() {
   run_once(1, 1, 3);
   for (unsigned i = 0; i < 5000; i += 200) {
     run_once(1500, i, 3);
+  }
+
+  run_cpp_once(1, 1, 3);
+  for (unsigned i = 0; i < 5000; i += 200) {
+    run_cpp_once(1500, i, 3);
   }
   return 0;
 }
