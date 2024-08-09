3fc4925bdbc5aeefd016afec555d908ae0de33cb
Hugh Wang <hghwng@gmail.com>
2024-08-09 11:30:08 +0800

    plot: refactor

--- analysis/plot.py
+++ analysis/plot.py
@@ -17,48 +17,53 @@ def read_data():
                 print("ERR:", line)
                 assert False
             xs = line[pos + SKIP :].split(",")
-            rows.append([xs[0], int(xs[1]), int(xs[2]), int(xs[3])])
+            rows.append([xs[0], float(xs[1]), float(xs[2]), float(xs[3])])
     df = pd.DataFrame.from_records(rows, columns=("Kind", "NItem", "SLen", "Duration"))
     df = df.pivot(columns=["Kind"], index=["SLen", "NItem"])
     df.columns = df.columns.droplevel(0)
     return df
 
 
-C_INNER = "C++ → Inner API"
-C_C = "C++ → C API"
-C_ARKVM = "C++ → NAPI (ArkVM)"
-C_NODEJS = "C++ → NAPI (NodeJS)"
-F_NAPI = "Flutter → N API"
+C_INNER = "InnerAPI"
+C_C = "C API → InnerAPI"
+C_ARKVM = "NAPI (ArkVM) → InnerAPI"
+C_NODEJS = "NAPI (NodeJS) → InnerAPI"
+C_TAIHE = "Taihe → InnerAPI"
+F_NAPI = "Flutter → NAPI"
 F_CAPI = "Flutter → C API"
 F_TAIHE = "Flutter → Taihe"
 
+C_ALL = [C_INNER, C_TAIHE, C_C, C_ARKVM, C_NODEJS]
+F_ALL = [F_NAPI, F_CAPI, F_TAIHE]
+NEW_COLUMNS = {
+    "x86-cpp-cpp": C_INNER,
+    "x86-c-capi": C_C,
+    "x86-cpp-taihe": C_TAIHE,
+    "arkts-napi": C_ARKVM,
+    "x86-nodejs-napi": C_NODEJS,
+    "dart-napi": F_NAPI,
+    "x86-dart-capi": F_CAPI,
+    "x86-dart-taihe": F_TAIHE,
+}
+
 
 def normalize(df: pd.DataFrame):
-    # Cast all x86 to aarch64 based on C-API.
-    x86_to_aarch64_factor = df["c-capi"] / df["x86-c-capi"]
+    # Cast all x86 to aarch64 based on InnerAPI.
+    x86_to_aarch64_factor = (df["cpp-cpp"] / df["x86-cpp-cpp"]).mean()
     for row_name in df.columns.values:
         assert isinstance(row_name, str)
         if row_name.startswith("x86-"):
             df[row_name] *= x86_to_aarch64_factor
 
     # Rename and reorder columns.
-    del df["c-capi"]
-    new_columns = {
-        "x86-cpp-cpp": C_INNER,
-        "x86-c-capi": C_C,
-        "arkts-napi": C_ARKVM,
-        "x86-nodejs-napi": C_NODEJS,
-        "dart-napi": F_NAPI,
-        "x86-dart-capi": F_CAPI,
-        "x86-dart-taihe": F_TAIHE,
-    }
-    df = df[new_columns.keys()].rename(new_columns, axis=1)  # type:ignore
+    df = df[NEW_COLUMNS.keys()].rename(NEW_COLUMNS, axis=1)  # type:ignore
 
     # Separate the benchmark for context switching.
     r = df.loc[0, 1500]
     df.drop(index=r.name, inplace=True)
-    r = df.loc[1, 1]
+    r = df.loc[1, 0]
     df.drop(index=r.name, inplace=True)
+    r.name = 'Call Duration'
 
     # Drop the boring "NItem", which always equals to 1500.
     df.index = df.index.droplevel(1)
@@ -70,10 +75,10 @@ def plot(df: pd.DataFrame):
     import matplotlib.pyplot as plt
 
     # Boxplot
-    x = df[[F_CAPI, F_TAIHE]].apply(lambda x: x / df[C_INNER])
+    x = df[F_ALL].apply(lambda x: x / df[C_INNER])
 
     fig, ax = plt.subplots(1, 1)
-    sns.boxplot(x, ax=ax)
+    sns.barplot(x, ax=ax)
     print(x.describe())
 
     # plt.yscale('log')
@@ -84,9 +89,10 @@ def plot(df: pd.DataFrame):
 def main():
     df = read_data()
     df_startup, df_main = normalize(df)
-    # print(df_startup)
-
-    plot(df_main)
+    df = df_main
+    del df_startup
+    print(df)
+    plot(df)
 
 
 if __name__ == "__main__":
