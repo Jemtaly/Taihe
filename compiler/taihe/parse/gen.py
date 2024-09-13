b467515807246c365aeb325cbf5b025aa64da2e4
Mingzhe <asanrocks@outlook.com>
2024-09-13 10:05:51 +0800

    parse/gen: Compiler

--- compiler/taihe/parse/gen.py
+++ compiler/taihe/parse/gen.py
@@ -1,37 +1,60 @@
-from io import StringIO
 from typing import Iterable, TextIO
+from pathlib import Path
+from subprocess import check_call
 
 
 class AntlrBuilder:
-    """Builds ANTLR grammar file with ease."""
+    """Writes ANTLR grammar file to a buffer."""
 
-    def __init__(self, buf: TextIO):
+    def __init__(self, buf: TextIO, name: str):
         self.buf = buf
-
-    def grammar(self, name: str):
         print(f"grammar {name};", file=self.buf)
         print(file=self.buf)
 
     def rule(self, name: str, rule: str | Iterable[str]):
         print(name, file=self.buf)
         if isinstance(rule, str):
-            r = (rule,)
+            rs = (rule,)
+        else:
+            rs = rule
 
         is_first = False
-        for r in rule:
-            prefix = "\t:" if is_first else "\t|"
+        for r in rs:
+            prefix = "\t|" if is_first else "\t:"
             print(prefix, r, file=self.buf)
             is_first = True
         print("\t;", file=self.buf)
         print(file=self.buf)
 
 
+class AntlrCompiler(AntlrBuilder):
+    """Writes ANTLR grammar file to a directory and complies it."""
+
+    def __init__(self, out_dir: Path, name: str):
+        self._out_dir = out_dir
+        self._g4_path = out_dir / f"{name}.g4"
+        f = open(self._g4_path, "w")
+        super().__init__(f, name)
+
+    def compile(self):
+        self.buf.close()
+        args = [
+            "antlr4",
+            "-Dlanguage=Python3",
+            "-no-listener",
+            self._g4_path,
+        ]
+        check_call(args)
+
+
 def main():
-    buf = StringIO()
-    b = AntlrBuilder(buf)
-    b.grammar("Taihe")
-    b.rule("fragment LETTER", ["_", "'A' .. 'Z'"])
-    print(buf.getvalue())
+    p = Path("./antlr-gen")
+    p.mkdir(exist_ok=True)
+    b = AntlrCompiler(p, "Taihe")
+    b.rule("specification", "SEMI")
+    b.rule("fragment LETTER", ["'_'", "'A' .. 'Z'"])
+    b.rule("SEMI", "';'")
+    b.compile()
 
 
 if __name__ == "__main__":
